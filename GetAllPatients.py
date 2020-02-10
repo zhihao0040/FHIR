a9b55859ce4dd2a65a5d7b4164d520ffb854adbb
zhihao0040 <58205698+zhihao0040@users.noreply.github.com>
2020-02-10 18:30:24 +1100

    Add files via upload
    Added comments

--- GetAllPatients.py
+++ GetAllPatients.py
@@ -1,19 +1,48 @@
+###
+"""
+A Jupyter Notebook is not used because the amount of output slows down the 
+Jupyter notebook by a lot and crashes it.
+
+This python script should serve only as an example of querying for pages. 
+The current approach is not scalable as one needs to type out all possible fields.
+
+The current approach goes through each type of resource each page at a time to visit every single detail.
+1) Patient
+2) Observation
+3) Immunization
+4) DiagnosticReport
+and so on
+"""
+
+
 import requests
 import pandas as pd
 
+
+### Base URL - The base URL used when extracting the data
+BASE_URL = "http://hapi-fhir.erc.monash.edu:8080/baseDstu3/"
+# BASE_URL = "http://hapi.fhir.org/baseR4/"
+
 def checkHasNext(jsonPage):
+    """
+    Returns a boolean indicating whether the current jsonPage has a link to the next page.
+    :param jsonPage: The current json string returned from querying a page
+    :timecomplexity: O(1) since there are only 3 items in jsonPage['link'] at max
+    :return: a Boolean indicating if the current jsonPage still has a next page
+    """
     for i in range(len(jsonPage['link'])):
         if jsonPage['link'][i]['relation'] == "next":
             return True
     return False
 
-
-patientPage = requests.get("http://hapi-fhir.erc.monash.edu:8080/baseDstu3/Patient").json()
+# Starting off with Patient's basic details, in order to obtain Patient id
+patientPage = requests.get(BASE_URL + "Patient").json()
 df = pd.DataFrame(columns = ['patientId', 'birthDate'])
 
 
 ##########################################################################################################
-
+# These are strings that are used as keys 
+# DataFrame's columns relating to patient initialized here
 hasNoNextLinkBoolean = False
 currentPage = patientPage
 PATIENT_MOTHER_MAIDEN_NAME = "patient-mothersMaidenName"
@@ -38,17 +67,16 @@ df["birthDate"] = ""
 df["maritalStatus"] = ""
 df["multipleBirthBoolean"] = ""
 
-while not (hasNoNextLinkBoolean):
-    #     currentPage = currentPage.json()
-    #     print(currentPage)
-    try:
-        for i in range(len(currentPage['entry'])):
+while not (hasNoNextLinkBoolean): # while not at last page, will not skip last page as the boolean will be set to false at the bottom only
+    try: # these code and access to data are obtained by looking the structure of the page as shown in https://www.hl7.org/fhir/patient.html
+        for i in range(len(currentPage['entry'])): # for each entry
             currentPageRes = currentPage['entry'][i]['resource']
             patientId = currentPageRes['id']
-            #             print(patientId)
             df.loc[len(df.index), 'patientId'] = patientId
 
             for j in range(len(currentPageRes['extension'])):
+
+                ## Populating the info that is to be added into the dataframe
                 extUrl = currentPageRes['extension'][j]['url']
                 extUrlCore = extUrl[len(extUrlHeader):]
                 extUrlCore2 = extUrl[len(extUrlHeader2):]
@@ -114,7 +142,10 @@ while not (hasNoNextLinkBoolean):
                     patientInfo = currentPageRes['extension'][j]['valueDecimal']
 
                     df.loc[(df.index[df['patientId'] == patientId])[0]][extUrlCore2] = patientInfo
-
+                
+                ##
+            
+            ## Putting the data into the dataframe based on patientId
             patientGender = currentPageRes["gender"]
             patientBirthDate = currentPageRes["birthDate"]
             patientMaritalStatus = currentPageRes["maritalStatus"]['coding'][0]['display']
@@ -127,29 +158,40 @@ while not (hasNoNextLinkBoolean):
             df.loc[(df.index[df['patientId'] == patientId])[0]]["maritalStatus"] = patientMaritalStatus
 
             df.loc[(df.index[df['patientId'] == patientId])[0]]["multipleBirthBoolean"] = patientMultipleBirthBoolean
+            
+            ##
 
 
-
-    except KeyError as error:
+    except KeyError as error: ## Sometimes the page doesn't fully follow the structure as defined in https://www.hl7.org/fhir/patient.html
+                              ## for e.g. when the patient doesn't have their birhdates recorded and etc.
         print(error)
 
-    if not checkHasNext(currentPage):
+    if not checkHasNext(currentPage):  ## if don't have next page, set hasNoNextLinkBoolean to true
         hasNoNextLinkBoolean = True
         currentPage = requests.get(currentPage['link'][1]['url']).json()
 
     else:
         currentPage = requests.get(currentPage['link'][1]['url']).json()
 
-print(df)
+# print(df)
 
 ##########################################################################################################
-print(df.loc[df["patientId"] == "82821"])
-print(df.loc[df['patientId'] == 71525])
+# print(df.loc[df["patientId"] == "82821"])
+# print(df.loc[df['patientId'] == 71525])
 
-observationPage = requests.get("http://hapi-fhir.erc.monash.edu:8080/baseDstu3/Observation").json()
+observationPage = requests.get(BASE_URL + "Observation").json()
 
 ##########################################################################################################
+# I converted most units to 10^0 at the time but shouldn't do this but rather include inside the name of the field the prefix for e.g. totalCholestrol (mg)
 def unitConversion(value, fromUnit="noPrefix", toUnit="noPrefix"):
+    """
+    Ad-hoc way of allowing conversion of units from one prefix to the other
+    :param value: The value in the current unit
+    :param fromUnit: The current prefix of the value
+    :param toUnit: The unit to change the current value to
+    :timecomplexity: O(1) 
+    :return: the value with its 'value' changed to the current unit
+    """
     prefix = {'y': 1e-24,  # yocto
               'z': 1e-21,  # zepto
               'a': 1e-18,  # atto
@@ -194,9 +236,14 @@ def unitConversion(value, fromUnit="noPrefix", toUnit="noPrefix"):
     return value / (toUnit / fromUnit)
 
 
-def getValueString(observationEntryResource):
-    for key in observationEntryResource.keys():
-#         print(key)
+def getValueString(observationEntryResource): # as shown in https://www.hl7.org/fhir/patient.html, valueStrings can be in multiple formats
+    """
+    Based on https://www.hl7.org/fhir/patient.html, identified all possible valueStrings. Ignoring some like 'time periods' for now
+    :param observationEntryResource: BASE_URL/Patient.json()['entry'][i]['resource']
+    :timecomplexity: O(1) 
+    :return: the type of valueString
+    """  
+    for key in observationEntryResource.keys(): # should only have 1 key, keys() is in []
         if key == "valueQuantity":
             return key
         elif key == "valueCodeableConcept":
@@ -221,6 +268,13 @@ def getValueString(observationEntryResource):
             return key
 ##########################################################################################################
 def upadteDFWithObservationValueOfPatient(df, patientId, observationEntryResource):
+    """
+    Update df at the patientId's row with the info to update
+    :param df: the dataframe to be updated 
+    :param patientId: the row/patient info to be updated
+    :param observationEntryResource: BASE_URL/Patient.json()['entry'][i]['resource']
+    :timecomplexity: df[loc]'s time complexity
+    """  
     for key in observationEntryResource.keys():
 #         print(key)
         if key == "valueQuantity":
@@ -259,11 +313,16 @@ def upadteDFWithObservationValueOfPatient(df, patientId, observationEntryResourc
 ##########################################################################################################
 
 def upadteDFWithObservationColumnNames(df, jsonPage):
-    counter = 0
+    """
+    Update df with column names by going through each page 
+    :param df: The dataframe to be updated
+    :jsonPage: The whole json string returned when querying a page
+    """  
+    # counter = 0
     hasNoNextLinkBoolean = False
 
-    while not (hasNoNextLinkBoolean):
-        counter += len(jsonPage['entry'])
+    while not (hasNoNextLinkBoolean): 
+        # counter += len(jsonPage['entry'])
         for i in range(len(jsonPage['entry'])):
             observationEntryResource = jsonPage['entry'][i]['resource']
             #             print(str(observationEntryResource))
@@ -275,7 +334,7 @@ def upadteDFWithObservationColumnNames(df, jsonPage):
             #         print("\n")
             #         print(observationEntryResource.keys())
             #         print(observationEntryResource['code']['text'])
-            df[observationEntryResource['code']['text']] = ""
+            df[observationEntryResource['code']['text']] = "" # Update df with various field names
 
         if not checkHasNext(jsonPage):
             hasNoNextLinkBoolean = True
@@ -283,7 +342,7 @@ def upadteDFWithObservationColumnNames(df, jsonPage):
 
         else:
             jsonPage = requests.get(jsonPage['link'][1]['url']).json()
-            print(counter)
+            # print(counter)
 
 
 # print(observationPage)
@@ -295,18 +354,15 @@ hasNoNextLinkBoolean = False
 currentPage = observationPage
 
 
-while not (hasNoNextLinkBoolean):
-    #     currentPage = currentPage.json()
-    #     print(currentPage)
+while not (hasNoNextLinkBoolean): # while not at last page, will not skip last page as the boolean will be set to false at the bottom only
     try:
 
-        for i in range(len(currentPage['entry'])):
+        for i in range(len(currentPage['entry'])): # these code and access to data are obtained by looking the structure of the page as shown in https://www.hl7.org/fhir/observation.html
             currentPageRes = currentPage['entry'][i]['resource']
             patientId = currentPageRes['subject']
             effectiveDateTime = currentPageRes["effectiveDateTime"]
             valueStringKey = getValueString(currentPageRes)
 
-            #             print(patientId)
             df.loc[len(df.index), 'patientId'] = patientId
 
             for j in range(len(currentPageRes['extension'])):
@@ -376,6 +432,9 @@ while not (hasNoNextLinkBoolean):
 
                     df.loc[(df.index[df['patientId'] == patientId])[0]][extUrlCore2] = patientInfo
 
+            # Update df with info -- not completed cause this method is deemed unscalable for now
+            # looking into Google/Fhir
+            # change accordingly below as shown in the same code above.
             patientGender = currentPageRes["gender"]
             patientBirthDate = currentPageRes["birthDate"]
             patientMaritalStatus = currentPageRes["maritalStatus"]['coding'][0]['display']
